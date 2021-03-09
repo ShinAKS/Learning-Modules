293292b3c9b8e53768f1a3de35404bf53589c05d
ShinAKS <ayushsinha0@gmail.com>
2021-03-09 23:11:27 +0530

    Initial commit

---
+++ super-mario-themed-map/script.js
@@ -0,0 +1,7 @@
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 34.6685254567688, lng: 135.43017199575203 },
+    zoom: 18,
+    mapId: "d560e7e2ab181bfb",
+  });
+}
